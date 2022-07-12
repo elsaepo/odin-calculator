7394d96564d68d3e47929939c3401efc2bfdda5f
Carl Madsen <madsencarlb@gmail.com>
2022-07-13 00:18:55 +1000

    add simple calculator functions. allow to link operators without equating

--- calculator.js
+++ calculator.js
@@ -1,3 +1,12 @@
+const buttonNumbers = document.querySelectorAll(".number");
+const buttonOperators = document.querySelectorAll(".operator");
+const buttonEquals = document.querySelector("#button-equals");
+const buttonClear = document.querySelector("#button-clear");
+let outputDisplay = document.querySelector("#output");
+
+
+// Operator functions
+
 function add(num1, num2){
     return num1 + num2;
 }
@@ -16,3 +25,51 @@ function divide(num1, num2){
     };
     return num1 / num2;
 }
+
+function operate(operator, num1, num2){
+    return operator(num1, num2);
+}
+
+
+let numCurrent = "";
+let numTotal = 0;
+let currentOperator;
+
+// equalButton is defined as a function so numTotal can be updated through multiple operator presses
+let equalButton = function(){
+    return operate(window[currentOperator], numTotal, Number(numCurrent));
+}
+
+
+// Button event listeners
+
+buttonNumbers.forEach(button => button.addEventListener("mousedown", function(event){
+    let thisNumber = event.target.getAttribute("id").slice(7);
+    numCurrent += thisNumber;
+    outputDisplay.textContent = numCurrent;
+}))
+
+buttonOperators.forEach(button => button.addEventListener("mousedown", function(event){
+    let thisOperator = event.target.getAttribute("id").slice(7);
+    if (numTotal && numCurrent){
+        numTotal = equalButton();
+    } else {
+        numTotal += Number(numCurrent);
+    }
+    currentOperator = thisOperator;
+    numCurrent = "";
+}))
+
+buttonEquals.addEventListener("mousedown", function(){
+    // window[...] uses bracket notation to access the method in window. Turns string into function
+    numTotal = equalButton();
+    numCurrent = "";
+    outputDisplay.textContent = numTotal;
+})
+
+buttonClear.addEventListener("mousedown", function(){
+    numCurrent = "";
+    numTotal = 0;
+    currentOperator = "";
+    outputDisplay.textContent = "";
+})
